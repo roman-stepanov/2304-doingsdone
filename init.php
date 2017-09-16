5c54816f87e34d94f2facb51d4fd5838bfe03830
Roman Stepanov <stepanov112@gmail.com>
2017-09-16 12:50:17 +0300

    Подключение к БД

---
+++ init.php
@@ -0,0 +1,9 @@
+<?php
+$connect = mysqli_connect('localhost', 'root', '', 'doingsdone');
+
+if ($connect == false) {
+    $error = render_template('templates/error.php', ['error' => mysqli_connect_error()]);
+    print($error);
+    exit();
+}
+?>
